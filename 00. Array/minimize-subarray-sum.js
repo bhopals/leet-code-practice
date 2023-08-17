6820600310356701f996cf582aae3c7deeb63cc4
Bhopal Singh <bhopalsinghsis@gmail.com>
2023-08-17 10:22:43 -0400

    Minimize Subarray

---
+++ 00. Array/minimize-subarray-sum.js
@@ -0,0 +1,44 @@
+// Minimum Size Subarray Sum
+
+// Solution
+// Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+// Example 1:
+
+// Input: target = 7, nums = [2,3,1,2,4,3]
+// Output: 2
+// Explanation: The subarray [4,3] has the minimal length under the problem constraint.
+// Example 2:
+
+// Input: target = 4, nums = [1,4,4]
+// Output: 1
+// Example 3:
+
+// Input: target = 11, nums = [1,1,1,1,1,1,1,1]
+// Output: 0
+
+// Constraints:
+
+// 1 <= target <= 109
+// 1 <= nums.length <= 105
+// 1 <= nums[i] <= 104
+
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function (target, nums) {
+  let left = (sum = 0);
+  let minLength = Infinity;
+  for (let right = 0; right < nums.length; right++) {
+    sum += nums[right];
+    while (sum >= target) {
+      minLength = Math.min(minLength, right - left + 1);
+      sum -= nums[left];
+      left++;
+    }
+    //console.log(`left:${left}, right:${right}, sum:${sum}`);
+  }
+  return minLength === Infinity ? 0 : minLength;
+};
