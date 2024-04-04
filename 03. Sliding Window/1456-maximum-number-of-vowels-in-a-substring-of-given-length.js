d9a5efcc1cc43ccad8f935ce84a3b894a6d3e3c0
Bhopal Singh <bhopalsinghsis@gmail.com>
2024-04-04 06:29:11 -0400

    LeetCode - Maximum Number of Vowels in a Substring

--- 03. Sliding Window/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 03. Sliding Window/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -19,3 +19,38 @@
 // Input: s = "leetcode", k = 3
 // Output: 2
 // Explanation: "lee", "eet" and "ode" contain 2 vowels.
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function (s, k) {
+  let vowels = ["a", "e", "i", "o", "u"];
+  let vowelCount = 0;
+  let maxVowelCount = 0;
+  for (let i = 0; i < k; i++) {
+    if (vowels.includes(s[i])) {
+      vowelCount++;
+    }
+  }
+  console.log(`INIT -  vowelCount: ${vowelCount}`);
+  maxVowelCount = vowelCount;
+
+  //Iterate over the string +1 step
+  for (let i = k; i < s.length; i++) {
+    if (vowels.includes(s[i - k])) {
+      vowelCount--;
+    }
+
+    if (vowels.includes(s[i])) {
+      vowelCount++;
+    }
+
+    if (maxVowelCount < vowelCount) {
+      maxVowelCount = vowelCount;
+    }
+    // console.log(`InLoop - currItem:${currItem}, lastItem: ${lastItem}, vowelCount: ${vowelCount}, maxVowelCount: ${maxVowelCount}`);
+  }
+  return maxVowelCount;
+};
